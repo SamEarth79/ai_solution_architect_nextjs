bb811f42afc649119626e9a7559e9a5a7de2ddc9
Samarth M M <samarthmm7@gmail.com>
2025-03-18 11:34:13 +0530

    Added layers

--- src/components/ExcalidrawWrapper/ExcalidrawWrapper.tsx
+++ src/components/ExcalidrawWrapper/ExcalidrawWrapper.tsx
@@ -9,114 +9,81 @@ export const ExcalidrawWrapper: React.FC = () => {
 	const [excalidrawAPI, setExcalidrawAPI] = useState();
 	const blocks = {
 		"blocks": [
-			{
-				"block_id": 1,
-				"block_info_link": "https://en.wikipedia.org/wiki/Web_application",
-				"block_name": "Web Browser",
-				"block_type": 0,
-				"connected_blocks": [
-					2
-				]
-			},
-			{
-				"block_id": 2,
-				"block_info_link": "https://en.wikipedia.org/wiki/Load_balancing_(computing)",
-				"block_name": "Load Balancer",
-				"block_type": 7,
-				"connected_blocks": [
-					1,
-					3,
-					4
-				]
-			},
-			{
-				"block_id": 3,
-				"block_info_link": "https://reactjs.org/",
-				"block_name": "Frontend (React)",
-				"block_type": 1,
-				"connected_blocks": [
-					2,
-					5
-				]
-			},
-			{
-				"block_id": 4,
-				"block_info_link": "https://nodejs.org/en/",
-				"block_name": "Backend (Node.js)",
-				"block_type": 2,
-				"connected_blocks": [
-					2,
-					5,
-					6,
-					8,
-					9,
-					10
-				]
-			},
-			{
-				"block_id": 5,
-				"block_info_link": "https://en.wikipedia.org/wiki/API_gateway",
-				"block_name": "API Gateway",
-				"block_type": 8,
-				"connected_blocks": [
-					3,
-					4,
-					6,
-					10
-				]
-			},
-			{
-				"block_id": 6,
-				"block_info_link": "https://auth0.com/",
-				"block_name": "Authentication & Authorization (Auth0)",
-				"block_type": 6,
-				"connected_blocks": [
-					4,
-					5
-				]
-			},
-			{
-				"block_id": 7,
-				"block_info_link": "https://www.postgresql.org/",
-				"block_name": "Database (PostgreSQL)",
-				"block_type": 3,
-				"connected_blocks": [
-					8
-				]
-			},
-			{
-				"block_id": 8,
-				"block_info_link": "https://redis.io/",
-				"block_name": "Message Queue (Redis-Pub/Sub)",
-				"block_type": 10,
-				"connected_blocks": [
-					4,
-					7,
-					9
-				]
-			},
-			{
-				"block_id": 9,
-				"block_info_link": "https://prometheus.io/",
-				"block_name": "Logging & Monitoring (Prometheus/Grafana)",
-				"block_type": 9,
-				"connected_blocks": [
-					4,
-					8
-				]
-			},
-			{
-				"block_id": 10,
-				"block_info_link": "https://www.cloudamqp.com/",
-				"block_name": "Rate Limiter",
-				"block_type": 13,
-				"connected_blocks": [
-					4,
-					5
-				]
-			}
+		  {
+			"block_id": 1,
+			"block_info_link": "https://developer.apple.com/ios/",
+			"block_name": "iOS Client",
+			"block_type": 0,
+			"connected_blocks": [7],
+			"layer": 2
+		  },
+		  {
+			"block_id": 2,
+			"block_info_link": "https://developer.android.com/",
+			"block_name": "Android Client",
+			"block_type": 0,
+			"connected_blocks": [7],
+			"layer": 2
+		  },
+		  {
+			"block_id": 3,
+			"block_info_link": "https://www.example.com/webclient",
+			"block_name": "Web Client",
+			"block_type": 1,
+			"connected_blocks": [7],
+			"layer": 2
+		  },
+		  {
+			"block_id": 4,
+			"block_info_link": "https://firebase.google.com/docs/auth",
+			"block_name": "Authentication Service",
+			"block_type": 6,
+			"connected_blocks": [1, 2, 3, 7],
+			"layer": 3
+		  },
+		  {
+			"block_id": 5,
+			"block_info_link": "https://www.example.com/userservice",
+			"block_name": "User Service",
+			"block_type": 2,
+			"connected_blocks": [7, 6],
+			"layer": 3
+		  },
+		  {
+			"block_id": 6,
+			"block_info_link": "https://www.postgresql.org/",
+			"block_name": "User Database",
+			"block_type": 3,
+			"connected_blocks": [5],
+			"layer": 4
+		  },
+		  {
+			"block_id": 7,
+			"block_info_link": "https://www.example.com/messageservice",
+			"block_name": "Message Service",
+			"block_type": 2,
+			"connected_blocks": [1, 2, 3, 4, 5, 8, 9],
+			"layer": 3
+		  },
+		  {
+			"block_id": 8,
+			"block_info_link": "https://www.mongodb.com/",
+			"block_name": "Message Database",
+			"block_type": 4,
+			"connected_blocks": [7],
+			"layer": 4
+		  },
+		  {
+			"block_id": 9,
+			"block_info_link": "https://www.rabbitmq.com/",
+			"block_name": "Message Queue",
+			"block_type": 10,
+			"connected_blocks": [7],
+			"layer": 3
+		  }
 		]
-	};
+	  }
+	  
 
 	useEffect(() => {
 		const updateDimensions = () => {
@@ -134,31 +101,34 @@ export const ExcalidrawWrapper: React.FC = () => {
 	}, []);
 
 	useEffect(() => {
-		const processedElements = blocks.blocks.map((block) => {
-			return {
+		const processedElements = []
+		const layerWidths = [0, 0, 0, 0, 0];
+		const layerHeights = {
+			1: 0,
+			2: 120,
+			3: 240,
+			4: 360,
+			5: 480,
+		}
+		blocks.blocks.forEach((block) => {
+			const layer = block.layer;
+			const x = layerWidths[layer-1];
+			const y = layerHeights[layer];
+			const processedElement = {
 				type: "rectangle",
-				version: 141,
-				versionNonce: 361174001,
-				isDeleted: false,
 				id: `block-${block.block_id}`,
-				strokeWidth: 1,
-				fillStyle: "hachure",
-				strokeStyle: "solid",
-				roughness: 1,
+				x: x,
+				y: y,
+				width: 106.47265625,
+				height: 61.9765625,
+				version: 141,
 				roundness: {
 					type: 3,
 				},
-				opacity: 100,
-				angle: 0,
-				x: 100.50390625,
-				y: 93.67578125,
-				strokeColor: "#000000",
-				backgroundColor: "transparent",
-				width: 186.47265625,
-				height: 141.9765625,
-				seed: 1968410350,
-				groupIds: [],
 			}
+			layerWidths[layer-1] += 120;
+			processedElements.push(processedElement);
+			processedElements.push(processedElementText);
 		})
 		console.log(processedElements);
 		setInitialElements(processedElements);
@@ -192,28 +162,3 @@ export const ExcalidrawWrapper: React.FC = () => {
         </div>
     );
 };
-
-// {
-	// 	type: "rectangle",
-	// 	version: 141,
-	// 	versionNonce: 361174001,
-	// 	isDeleted: false,
-	// 	id: "oDVXy8D6rom3H1-LLH2-f",
-	// 	strokeWidth: 1,
-	// 	fillStyle: "hachure",
-	// 	strokeStyle: "solid",
-	// 	roughness: 1,
-	// 	roundness: {
-	// 	  type: 3,
-	// 	},
-	// 	opacity: 100,
-	// 	angle: 0,
-	// 	x: 100.50390625,
-	// 	y: 93.67578125,
-	// 	strokeColor: "#000000",
-	// 	backgroundColor: "transparent",
-	// 	width: 186.47265625,
-	// 	height: 141.9765625,
-	// 	seed: 1968410350,
-	// 	groupIds: [],
-//   },
